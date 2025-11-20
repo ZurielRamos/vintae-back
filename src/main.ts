fa49f3af2e287ebb480838f4d41f0c32966ed669
Zuriel Ramos <vzurielrc@gmail.com>
2025-11-20 10:53:56 -0500

    vercel

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+// 1. Importa SwaggerCustomOptions
+import { DocumentBuilder, SwaggerModule, SwaggerCustomOptions } from '@nestjs/swagger';
 
 async function bootstrap() {
-  // Crea la aplicación de forma estándar
   const app = await NestFactory.create(AppModule);
 
-  // Configuración de prefijo global
   app.setGlobalPrefix('api/v1');
 
-  // Configuración de Pipes de validación
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
@@ -19,17 +17,13 @@ async function bootstrap() {
     }),
   );
 
-  // Configuración de CORS
   app.enableCors({
-    origin: [
-      'http://localhost:5173',
-      'http://localhost:3000'
-    ], 
+    origin: ['http://localhost:5173', 'http://localhost:3000', 'https://vintae.vercel.app', 'https://vesster.store', 'https://adm.vesster.store'], 
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
 
-  // Configuración de Swagger
+  // --- CONFIGURACIÓN SWAGGER CDN ---
   const config = new DocumentBuilder()
     .setTitle('Design API')
     .setDescription('API para gestionar productos y diseños.')
@@ -38,11 +32,24 @@ async function bootstrap() {
     .build();
     
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api/docs', app, document);
 
-  // Inicia el servidor escuchando en el puerto definido
+  // 2. Definimos las opciones para usar CDN externa (unpkg)
+  const customOptions: SwaggerCustomOptions = {
+    customCssUrl: 'https://unpkg.com/swagger-ui-dist@5.17.14/swagger-ui.css',
+    customJs: [
+      'https://unpkg.com/swagger-ui-dist@5.17.14/swagger-ui-bundle.js',
+      'https://unpkg.com/swagger-ui-dist@5.17.14/swagger-ui-standalone-preset.js',
+    ],
+    swaggerOptions: {
+      persistAuthorization: true,
+    },
+  };
+
+  // 3. Pasamos las opciones al setup
+  SwaggerModule.setup('api/docs', app, document, customOptions);
+  // ---------------------------------
+
   await app.listen(process.env.PORT ?? 3000);
-  console.log(`Application is running on: ${await app.getUrl()}`);
 }
 
 bootstrap();
